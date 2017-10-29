e19eb85728e369c3c1c8bd72a8059dff4a1d02e0
Gabriel Coelho Soares <soares@workmail.com>
2017-10-29 15:07:39 -0200

    last save

--- novos projetos/js-exercises/kebab-to-snake.js
+++ novos projetos/js-exercises/kebab-to-snake.js
@@ -3,3 +3,7 @@
 *write a function kebabToSnake() wich takes a single kebab-cased
 *string argument and returns the snake_cased version.
 **/
+function kebabToSnake(changeString){
+  var newString = changeString.replace(/-/g, "_");
+  return newString;
+}
